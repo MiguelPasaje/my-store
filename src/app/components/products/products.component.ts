8f2b33058296c4e56b4f75e6cdfd97faf17f47dc
MiguelPasaje <cartel-dy@hotmail.com>
2023-03-30 21:33:59 -0500

    delete

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -142,4 +142,14 @@ export class ProductsComponent {
     })
   }
 
+  deleteProduct(){
+    const id = this.productChosen.id;
+    this.productsService.delete(id)
+    .subscribe(()=>{
+      const productIndex = this.products.findIndex(item => item.id === this.productChosen.id)
+      this.products.splice(productIndex,1)
+      this.showProductDetail = false;
+    })
+  }
+
 }
