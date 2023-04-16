7edf382cb48197121d1e5c8abb39609dc9f31a54
MiguelPasaje <cartel-dy@hotmail.com>
2023-04-16 15:57:06 -0500

    restructuracion para evitar callback hell

--- src/app/pages/category/category.component.ts
+++ src/app/pages/category/category.component.ts
@@ -2,10 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/models/product.model';
 import { ProductsService } from 'src/app/services/products.service';
+import { switchMap } from 'rxjs/operators'
 
 @Component({
   selector: 'app-category',
-  templateUrl: './category.component.html',
+  template: '<app-products [products]="products"  (loadMore)="onLoadMore()"></app-products>',
+  /* templateUrl: './category.component.html', */
   styleUrls: ['./category.component.scss']
 })
 export class CategoryComponent implements OnInit {
@@ -17,21 +19,35 @@ export class CategoryComponent implements OnInit {
 
   constructor(
     private route:ActivatedRoute,
-    private productService:ProductsService
+    private productsService:ProductsService
   ){ }
 
   ngOnInit():void{
-    this.route.paramMap.subscribe(params => {
-      this.categoryId = params.get('id')// 'id' este nombre debe estar giual en app-routing -> :id **1
-      console.log(this.categoryId,'id category url')
-      if (this.categoryId) {
-        this.productService.getByCategory(this.categoryId,this.limit,this.offset)
-        .subscribe(data =>{
-          this.products = data
-          console.log(data)
-        })
-      }
+    this.route.paramMap
+    .pipe(
+      switchMap(params =>{
+        this.categoryId = params.get('id')// 'id' este nombre debe estar giual en app-routing -> :id **1
+        console.log(this.categoryId,'id category url')
+        if (this.categoryId) {
+          return this.productsService.getByCategory(this.categoryId,this.limit,this.offset)
+        }
+        return []
 
+      })
+    )
+    .subscribe(data => {
+      this.products = data
     })
+  }
+
+  onLoadMore() {
+    if (this.categoryId) {
+      this.productsService
+        .getByCategory(this.categoryId, this.limit, this.offset)
+        .subscribe((data) => {
+          this.products = this.products.concat(data);
+          this.offset += this.limit;
+        });
+    }
   }
 }
