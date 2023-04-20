2e124ced7c481e611af654bb4fe1b048f9e43289
MiguelPasaje <cartel-dy@hotmail.com>
2023-04-19 21:51:50 -0500

    website Module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,66 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { HomeComponent } from './website/pages/home/home.component';
-import { NotFoundComponent } from './website/pages/not-found/not-found.component';
-import { CategoryComponent } from './website/pages/category/category.component';
-import { MycartComponent } from './website/pages/mycart/mycart.component';
-import { LoginComponent } from './website/pages/login/login.component';
-import { RegisterComponent } from './website/pages/register/register.component';
-import { RecoveryComponent } from './website/pages/recovery/recovery.component';
-import { ProfileComponent } from './website/pages/profile/profile.component';
-import { ProductDetailComponent } from './website/pages/product-detail/product-detail.component';
-import { LayoutComponent } from './website/components/layout/layout.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 
 const routes: Routes = [
   //crear reglas
   {
     path: '',
-    component: LayoutComponent,
-    children:[
-      {
-        path: '',
-        redirectTo: 'home',
-        pathMatch:'full'
-      },
-      {
-        path: 'home',
-        component: HomeComponent
-      },
-      {
-        path: 'category/:id', // id debe llamrse igual en donde se vaya a utilizar **1
-        component: CategoryComponent
-      },
-      {
-        path: 'product/:id', // id debe llamrse igual en donde se vaya a utilizar **1
-        component: ProductDetailComponent
-      },
-      {
-        path: 'notFoundComponent',
-        component: NotFoundComponent
-      },
-      {
-        path: 'mycartComponent',
-        component: MycartComponent
-      },
-      {
-        path: 'loginComponent',
-        component: LoginComponent
-      },
-      {
-        path: 'registerComponent',
-        component: RegisterComponent
-      },
-      {
-        path: 'recoveryComponent',
-        component: RecoveryComponent
-      },
-      {
-        path: 'profileComponent',
-        component: ProfileComponent
-      },
-    ]
+    loadChildren:()=> import('./website/website.module').then(m => m.WebsiteModule)
   },
   {
     path: 'cms',
---
+++ src/app/website/website-routing.module.ts
@@ -0,0 +1,69 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { HomeComponent } from './pages/home/home.component';
+
+import { CategoryComponent } from './pages/category/category.component';
+import { MycartComponent } from './pages/mycart/mycart.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { RecoveryComponent } from './pages/recovery/recovery.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
+import { LayoutComponent } from './components/layout/layout.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full',
+      },
+      {
+        path: 'home',
+        component: HomeComponent,
+      },
+      {
+        path: 'category/:id', // id debe llamrse igual en donde se vaya a utilizar **1
+        component: CategoryComponent,
+      },
+      {
+        path: 'product/:id', // id debe llamrse igual en donde se vaya a utilizar **1
+        component: ProductDetailComponent,
+      },
+      /*       {
+        path: 'notFoundComponent',
+        component: NotFoundComponent
+      }, */
+      {
+        path: 'mycartComponent',
+        component: MycartComponent,
+      },
+      {
+        path: 'loginComponent',
+        component: LoginComponent,
+      },
+      {
+        path: 'registerComponent',
+        component: RegisterComponent,
+      },
+      {
+        path: 'recoveryComponent',
+        component: RecoveryComponent,
+      },
+      {
+        path: 'profileComponent',
+        component: ProfileComponent,
+      },
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class WebsiteRoutingModule {}
